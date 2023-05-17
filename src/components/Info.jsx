585477b456536f240434044e2b2be2800a3894d1
Juan Manuel Luque <94568781+juanmluq@users.noreply.github.com>
2023-05-17 13:01:13 -0300

    Update Info.jsx

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import "./Info.css"
+import "./Info.css";
+import clip1 from "../videos/clip1.mp4"
+import clip2 from "../videos/clip2.mp4"
 
 export default function Info() {
   return (
     <div className='centradoinfo'>
       <br />      
-      {/* { <video controls>
-          <source src="https://player.vimeo.com/video/338795767?h=9e40727958&amp;muted=1&amp;autoplay=1&amp;loop=1&amp;transparent=0&amp;background=1&amp;app_id=122963" />
-        </video> }  */}
-      {/* <img className='imageinfo' src="https://media.tenor.com/i3qZzUkQT6AAAAAd/casino.gif" alt="gif" /> */}
+     
         <h3>Info Videos</h3>
         <br />
         <div>
@@ -19,8 +18,23 @@ export default function Info() {
           Videos:
         <br />
         <br />
-        Cargando...
-        </div>
+        El siguiente video informa como comprar monedas:
+        <br />
+ { <video controls className='vid'>
+          <source  src={clip1} />
+        </video> } 
+        <br />
+        <br />
+        El siguiente video informa como vender una moneda:
+        <br />
+        { <video controls className='vid'>
+          <source  src={clip2} />
+        </video> } 
+
+        {/* { <video controls>
+          <source src="https://player.vimeo.com/video/338795767?h=9e40727958&amp;muted=1&amp;autoplay=1&amp;loop=1&amp;transparent=0&amp;background=1&amp;app_id=122963" />
+        </video> }  */}
+      </div>
     </div>
   );
 }
